b912ff61aa7c526a242b79c67f8aa718938c669f
javiernede <61659947+javiernede@users.noreply.github.com>
2022-01-12 08:47:39 -0300

    Desafío
    Desafío de la clase 2

---
+++ condiciones.js
@@ -0,0 +1,23 @@
+console.log("condicionales");
+
+let variable = true;
+
+if (variable) {
+    console.log("ejecucion");
+}
+
+if(false){
+    console.log("marvel");
+}
+if(5 == 6){ /* igualdad */
+    console.log("Hola a todos"); 
+}
+
+if("6"==6){ /* compara con dos signos si entra.*/
+    console.log("Hola a todos 1");
+}
+
+
+if("6"=== 6){ /* compara con tres signos NO entra.*/
+    console.log("Hola a todos 1");
+}
