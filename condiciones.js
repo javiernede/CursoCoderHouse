5e41eca1fe9ca379230024a67ca8b86a1defaf4c
javiernede <61659947+javiernede@users.noreply.github.com>
2022-01-12 10:21:52 -0300

    Update condiciones.js

--- condiciones.js
+++ condiciones.js
@@ -6,18 +6,19 @@ if (variable) {
     console.log("ejecucion");
 }
 
-if(false){
+if (false) {
     console.log("marvel");
 }
-if(5 == 6){ /* igualdad */
-    console.log("Hola a todos"); 
+if (5 == 6) { /* igualdad */
+    console.log("Hola a todos");
 }
 
-if("6"==6){ /* compara con dos signos si entra.*/
+if ("6" == 6) { /* compara con dos signos si entra.*/
     console.log("Hola a todos 1");
 }
 
 
-if("6"=== 6){ /* compara con tres signos NO entra.*/
+if ("6" === 6) { /* compara con tres signos NO entra.*/
     console.log("Hola a todos 1");
+
 }
