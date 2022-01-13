f81bd7f02dcc05a40434ad90a9615a1989ff2569
javiernede <61659947+javiernede@users.noreply.github.com>
2022-01-13 19:53:14 -0300

    Desafio de condicionales

---
+++ EjericioCondicional/condicionales.js
@@ -0,0 +1,14 @@
+/*Compara dos numeros*/ 
+
+let valor1 = parseInt(prompt("Ingrese un numero por favor:"));
+
+let valor2 = parseInt(prompt("Ingrese otro numero por favor:"));
+
+
+if (valor1 > valor2 ) {
+    alert("El número ingresado:  " + valor1 + " es mayor al número ingresado: " + valor2 );
+}else if (valor1 == valor2) {
+    alert("El número ingresado:  " + valor1 + " es igual al número ingresado: " + valor2 );
+}else{
+    alert("El número ingresado:  " + valor1 + " es menor al número ingresado: " + valor2 );
+}
