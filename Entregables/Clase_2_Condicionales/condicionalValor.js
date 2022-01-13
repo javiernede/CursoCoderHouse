80a4114d55d9fb56be0047b8ec87481871ed711c
javiernede <61659947+javiernede@users.noreply.github.com>
2022-01-13 20:46:36 -0300

    Estructuracion del proyecto

--- Entregables/Clase_2_Condicionales/condicionalValor.js
+++ Entregables/Clase_2_Condicionales/condicionalValor.js
@@ -3,7 +3,7 @@
 
 /*Se le solicita al usuario ingresar un numero, si este numero es mayor a 1000 lo muestro con un alert*/
 
-let valor3 = parseInt(prompt("Ingrese un valor por favor:"));
+let valor3 = parseInt(prompt("Ingrese un valor por favor. Muestro el valor si es mayor a 1000 "));
 
 
 if (valor3 > 1000 ){
@@ -17,13 +17,17 @@ if (valor3 > 1000 ){
 }
 
 /*Se le solicita al usuario que ingrese una palaba, en el enunciado pide comparar la palabra Hola, si la comparacion es igual ==> lo muestro con un alert.*/
-let valor4 = prompt("Ingrese una palabra por favor");
+let valor4 = prompt("Ingrese una palabra por favor. Si la palabra ingresada es Hola la muestro");
 
 if (valor4 === "Hola"){
 
     alert("El texto ingresado es " + valor4);
 
 }
+
+
+
+
 
 
 
---
+++ Entregables/Clase_2_Condicionales/condicionalValorEntre.js
@@ -0,0 +1,7 @@
+/*Se le solicita al usuario ingresar un número. Si este esta en 10 y 50  ==>lo muestro con un alert. */ 
+let valor5 = parseInt(prompt("Ingrese una numero por favor. Muestro valor si esta entre 10 y 50."));
+
+if (valor5 >= 10 && valor5 <= 50 )
+{
+    alert("El numero")
+}
