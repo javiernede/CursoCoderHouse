3b828a54dcef31d946c91e8b2f5211ae62103c12
javiernede <61659947+javiernede@users.noreply.github.com>
2022-01-16 11:14:05 -0300

    Entregables_v01

--- Entregables/clase_3_iteraciones/iteraciones_3.js
+++ Entregables/clase_3_iteraciones/iteraciones_3.js
@@ -1,7 +1,7 @@
-let resultado;
-let valor = parseInt(prompt("Ingrese un valor por favor"));
 
-for(i = 0; i <valor; i++) {
+let valorConct = parseInt(prompt("Ingrese un valor por favor"));
+
+for(i = 0; i < valorConct; i++) {
 
    
     alert ("Hola");
